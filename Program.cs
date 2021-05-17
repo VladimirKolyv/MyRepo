48ebe703d23742adf0d97eb9467c1b2dccf97f70
VladimirKolyv <kolyvanov.vova@yandex.ru>
2021-05-17 13:31:12 +0300

    Первый коммит

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string [] s= { "555", "777", "999" };
+            int n = 0;
+            foreach (string s1 in args)
+            {
+                n++;
+                Console.WriteLine("Аргумент " + n.ToString() + "  " + s1.ToString());
+            }
+            Console.Read();
+        }
+
+    }
+
+}
